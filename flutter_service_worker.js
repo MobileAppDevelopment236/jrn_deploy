941202eeb416475d6ea80402524b35bee9fa92f5
MobileAppDevelopment236 <srinuk236.anna@gmail.com>
2025-11-21 08:25:27 +0530

    fix: remove missing assets/icons reference and rebuild

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "08e780e5a57fc918611c567595bf414c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "701f5f2e902ecba2aa9c871d74c6fab6",
+"flutter_bootstrap.js": "881370aa7532c8d0372855b3972e7ca7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
