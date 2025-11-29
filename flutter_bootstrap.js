9040d01fef34b1c035a7ccab504a8dd6ddea6c8e
MobileAppDevelopment236 <srinuk236.anna@gmail.com>
2025-11-29 09:15:52 +0530

    fix: complete password reset functionality with auth session fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2731507120"
+    serviceWorkerVersion: "3077386215"
   }
 });
