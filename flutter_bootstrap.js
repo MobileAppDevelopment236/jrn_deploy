7fcf84fd3aed6822a0cd8c729d7204415835a913
MobileAppDevelopment236 <srinuk236.anna@gmail.com>
2025-12-02 09:00:14 +0530

    fix: use recovery token to establish auth session before password update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4048997246"
+    serviceWorkerVersion: "365521293"
   }
 });
